d020ca7ca78e1f9e8f745fe0d859ad994e224d8f
athallam <50723109+athallam@users.noreply.github.com>
2019-07-11 19:49:32 -0500

    Update Roulette.py

--- Roulette.py
+++ Roulette.py
@@ -1,14 +1,20 @@
 import random
 
-myInput = input("red, black")
-computerInput = random.randint(1,2)
-
-if computerInput == 1:
-  computerSelection = "red"
-elif computerInput == 2:
-  computerSelection = "black"
-
-if computerSelection == myInput:
-  print("You win!")
-else:
-  print("You lose")
+keepplaying = True  
+while keepplaying == True:
+  myInput = input("red, black")
+  computerInput = random.randint(1,2)
+  
+  if computerInput == 1:
+    computerSelection = "red"
+  elif computerInput == 2:
+    computerSelection = "black"
+  
+  if computerSelection == myInput:
+    print("You win!")
+  else:
+    print("You lose")
+    
+  user_input = input("Do you want to continue playing?")
+  if user_input == "no":
+    keepplaying = False
